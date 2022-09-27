3132ed50f82d5dda5362a9d1ab1de21a6ce1a33e
Lev2307 <Abrosimow1@mail.ru>
2022-09-27 19:27:10 +0300

    write two tests to authentication

--- task_scheduler/authentication/tests.py
+++ task_scheduler/authentication/tests.py
@@ -1,3 +1,21 @@
 from django.test import TestCase
+from .models import MyUser
 
 # Create your tests here.
+class MyUserTests(TestCase):
+    def setUp(self):
+        self.myuser = MyUser.objects.create(username='admin', password='123', email='a@a.com', is_subscribed=False, is_staff=True, is_active=True, choose_sending='telegram')
+        self.myuser.save()
+    def test_created_user(self):
+        '''Проверка создания пользователя'''
+        myusers_old = MyUser.objects.count()
+        myuser = MyUser.objects.create(username='admin_fake', password='123fake', email='a_fake@a.com', is_subscribed=False, is_staff=False, is_active=True, choose_sending='email')
+        myusers_new = MyUser.objects.count()
+        self.assertTrue(myusers_old+1 == myusers_new)
+    
+    def test_user_choosesending_exists(self):
+        myuser = MyUser.objects.get(id=1)
+        choices = []
+        for choice in MyUser.SendingChoices.choices:
+            choices.append(choice[0])
+        self.assertTrue(myuser.choose_sending in choices)
